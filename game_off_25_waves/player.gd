ab5e1c654198054ab54fe1c03106a747d7664dcc
Keshav Sumitran <61526291+AgeOfCheese@users.noreply.github.com>
2025-11-09 12:27:15 -0800

    added transformations to waves + triangle wave

--- game_off_25_waves/player.gd
+++ game_off_25_waves/player.gd
@@ -3,6 +3,12 @@ extends CharacterBody2D
 var start_time: float
 var current_wave = 0
 var base_y = -999999
+
+var freq = 2
+var amp = 2
+var inv = 1
+
+
 func _ready() -> void:
 	start_time = Time.get_ticks_msec()/500.0
 
@@ -10,17 +16,22 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var cur_time = Time.get_ticks_msec()/500.0
 	var wave_pos = cur_time-start_time
+	$Trail.add_point(global_position)
 	match(current_wave):
 		0:
 			velocity = Vector2(2,0)
 		1:
-			velocity = Vector2(2,cos(wave_pos)*5)
+			velocity = Vector2(2,inv*cos(wave_pos*freq)*5*amp)
+		2:
+			velocity = Vector2(2, inv*amp*6*(wave_pos/(2*freq) - floor(wave_pos/(2*freq) + 0.5)))
+		3:
+			velocity = Vector2(2, inv*amp*5*sign((sin(PI*wave_pos*freq))))
 		4:
 			if base_y<0:
 				base_y = global_position.y
 				print(base_y)
 			if base_y>=0:
-				velocity = Vector2(2,fmod(wave_pos,1)*5)
+				velocity = Vector2(2,amp*fmod(wave_pos,1)*5)
 				if abs(velocity.y) <= 0.25:
 					print(1)
 					global_position.y = base_y
@@ -42,4 +53,19 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("saw_wave"):
 		current_wave = 4
 		base_y = -999999
-		
+	if event.is_action_pressed("amp_up"):
+		amp+=1
+	if event.is_action_pressed("amp_down"):
+		amp-=1
+	if event.is_action_released("amp_up") or event.is_action_released("amp_down"):
+		amp =2
+	if event.is_action_pressed("freq_up"):
+		freq+=1
+	if event.is_action_pressed("freq_down"):
+		freq-=1
+	if event.is_action_released("freq_up") or event.is_action_released("freq_down"):
+		freq = 2
+	if event.is_action_pressed("inv"):
+		inv*=-1
+	if event.is_action_released("inv"):
+		inv*=-1
